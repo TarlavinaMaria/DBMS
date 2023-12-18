9983fcaaa774d87e3f65a6f5c67074eef72bef7b
tarla <tarlavina50@mail.ru>
2023-12-18 21:39:09 +0700

    class done

---
+++ SELECT/SQLQuery1 - Student on Directions.sql
@@ -0,0 +1,23 @@
+SELECT 
+    Students.last_name AS'Фамилия', 
+	Students.first_name AS'Имя', 
+	Students.middle_name AS'Отчество', 
+	DATEDIFF(HOUR, Students.birth_date, GETDATE())/8766 AS 'Возраст',
+--	365(дней в году)*4(високосный год раз в 4 года) = 1460 +1(день) = 1461 /4 = 365,25 * 24 (часа) = 8766
+	
+	Groops.groop_name AS'Группа'
+	--Directions.direction_name AS'Направление'
+FROM   
+    Students, 
+	Groops,
+	Directions
+WHERE  
+    Students.[groop] = Groops.groop_id
+    AND Groops.groop_name = 'PD_212'
+	AND Groops.direction = Directions.direction_id
+--	AND Directions.direction_name = 'java development'
+	AND Directions.direction_name = 'Разработка программного обеспечения'
+	ORDER BY 
+		Students.last_name DESC;
+--	ASC - Ascending (в порядке возрастания)
+--	DESC - Descending(в порядке убывания)
