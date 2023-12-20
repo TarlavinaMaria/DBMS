c20ac8261e4e4780e196b49c5735ece18b7b1cdc
tarla <tarlavina50@mail.ru>
2023-12-20 22:36:47 +0700

    Coding

---
+++ SELECT/SQLQuery1 - Schedule.sql
@@ -0,0 +1,9 @@
+SELECT 
+	dbo.Disciplines.discipline_name AS'Дисциплина', 
+	dbo.Groups.group_name AS 'Группа',
+	dbo.Schedule.[date] AS 'Дата',
+	dbo.Schedule.[time] AS 'Время',
+	dbo.Teachers.last_name + dbo.Teachers.first_name + dbo.Teachers.middle_name AS 'Преподаватель',
+	dbo.Schedule.spent AS 'Проведено ' 
+FROM
+	dbo.Schedule, dbo.Groups, dbo.Disciplines, dbo.Teachers;
